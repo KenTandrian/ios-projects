2cbe111b4c8b44013a88b347c356a4210c37d6a8
KenTandrian <kenricktan11@gmail.com>
2023-05-31 23:34:01 +0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Kenrick Tandrian on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
