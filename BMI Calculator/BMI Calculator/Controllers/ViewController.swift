3393efdbf2c4d1d768c7e6159175e9d58b7d0280
KenTandrian <kenricktan11@gmail.com>
2023-06-13 23:56:49 +0700

    feat(app): add new BMI Calculator app

---
+++ BMI Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(heightSlider.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(weightSlider.value)
+    }
+}
+
