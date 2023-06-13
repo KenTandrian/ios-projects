317b9e63d930d9c1aa532c9467c2c83e1c7378f7
KenTandrian <kenricktan11@gmail.com>
2023-06-13 23:10:00 +0700

    feat(destini): add basic functionality

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps."),
+    ]
+    
+    var storyNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        if choice == "Take a left." {
+            storyNumber = 1
+        } else {
+            storyNumber = 2
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+    }
 }
 
